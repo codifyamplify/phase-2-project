d97f4cda49eeb9c16393876fdeded06dcaa5d423
David Whitney <codifyamplify@gmail.com>
2022-11-05 22:06:01 -0400

    created starter db.json and a basic Header component

---
+++ game-lister/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header(){
+    return (
+        // return JSX
+        <div>
+            <h1>Game Lister</h1>
+            <h3>Your one-stop-shop to keep track of your dream playlist of the games you are most excited about!</h3>
+        </div>
+    )
+}
+
+export default Header
