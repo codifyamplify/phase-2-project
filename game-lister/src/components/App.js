51cbabea7c3bafd82d952dcb7a19e4f4a87e7eae
David Whitney <codifyamplify@gmail.com>
2022-11-07 20:43:49 -0500

    began testing react-router-dom. creating a dummy component or two to experiment with paths.

--- game-lister/src/components/App.js
+++ game-lister/src/components/App.js
@@ -3,16 +3,33 @@ import React from "react"
 import Header from "./Header"
 import GamesList from "./GamesList"
 // import AddNewGame from "./AddNewGame"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Block from './Block';
+import PageNotFound from './PageNotFound';
 
 function App() {
   // console.log("inside app")
 
   return (
-    <div className="App">
-      <Header />
-      {/* <AddNewGame /> */}
-      <GamesList />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* This should display only Header */}
+          <Header />
+        </Route>
+        <Route exact path="/block">
+          <Block />
+          </Route>
+        <Route exact path="/games">
+          {/* this should display AddNewGame and GamesList */}
+          <GamesList />
+        </Route>  
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ game-lister/src/components/Block.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Block(){
+    return (
+        <div className="block">
+            <h1>Blockie Block Nonsense</h1>
+            <h3>lorem ipsum</h3>
+        </div>
+    )
+}
+
+export default Block
